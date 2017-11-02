2835607449e4c30ef1dd187ee30d359d6fca0c15
Arslan 'Ars2014' Sakhapov <mister.saxapoff@yandex.ru>
2017-11-02 20:10:30 +0500

    Fixed setup.py and .travis.yml

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(
             'Operating System :: Microsoft :: Windows',
             'Operating System :: Unix',
             'Operating System :: MacOS',
+            'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: Implementation :: PyPy',
         ]
 )
